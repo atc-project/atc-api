3a722e2dfc0658f75c806c2243097070049897c9
mrblacyk <kweinzettl@gmail.com>
2019-08-04 11:44:22 +0200

    Playing with CI/CD

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 sleep 5; 
 python3 ./manage.py makemigrations --noinput;
 python3 ./manage.py migrate --noinput;
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('dev', 'dev@dev.com', 'dev')" | python manage.py shell;
 python3 ./manage.py runserver 0.0.0.0:8000;
