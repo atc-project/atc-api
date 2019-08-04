88d4abcf52abbafaf74dd14f5db0ac6eb059f2f7
mrblacyk <kweinzettl@gmail.com>
2019-08-04 11:38:52 +0200

    Playing with CI/CD

--- startup.sh
+++ startup.sh
@@ -1,1 +1,4 @@
-sleep 5; python3 ./manage.py runserver 0.0.0.0:8000
+sleep 5; 
+python3 ./manage.py makemigrations --noinput;
+python3 ./manage.py migrate --noinput;
+python3 ./manage.py runserver 0.0.0.0:8000;
