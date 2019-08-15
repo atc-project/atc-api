53e7acfe8d2831592ae0e36c2f6b610e6dc14b91
m.aksenov <m.aksenov@bi.zone>
2019-08-15 23:03:59 +0300

    fix ci-cd

--- atc/viewsets.py
+++ atc/viewsets.py
@@ -6,79 +6,79 @@ import atc.serializers as serializers
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class PlatformViewSet(viewsets.ModelViewSet):
     queryset = models.Platform.objects.all()
     serializer_class = serializers.PlatformSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class LogTypeViewSet(viewsets.ModelViewSet):
     queryset = models.LogType.objects.all()
     serializer_class = serializers.LogTypeSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class ChannelViewSet(viewsets.ModelViewSet):
     queryset = models.Channel.objects.all()
     serializer_class = serializers.ChannelSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class ProviderViewSet(viewsets.ModelViewSet):
     queryset = models.Provider.objects.all()
     serializer_class = serializers.ProviderSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class VolumeViewSet(viewsets.ModelViewSet):
     queryset = models.Volume.objects.all()
     serializer_class = serializers.VolumeSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class LogFieldViewSet(viewsets.ModelViewSet):
     queryset = models.LogField.objects.all()
     serializer_class = serializers.LogFieldSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class StageViewSet(viewsets.ModelViewSet):
     queryset = models.Stage.objects.all()
     serializer_class = serializers.StageSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class EventIdViewSet(viewsets.ModelViewSet):
     queryset = models.EventID.objects.all()
     serializer_class = serializers.EventIdSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = models.Tag.objects.all()
     serializer_class = serializers.TagSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class ReferenceViewSet(viewsets.ModelViewSet):
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class LoggingPolicyViewSet(viewsets.ModelViewSet):
     queryset = models.LoggingPolicy.objects.all()
     serializer_class = serializers.LoggingPolicySerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class DataNeededViewSet(viewsets.ModelViewSet):
     queryset = models.DataNeeded.objects.all()
     serializer_class = serializers.DataNeededSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.AllowAny,)
 
 
 class EnrichmentViewSet(viewsets.ModelViewSet):
