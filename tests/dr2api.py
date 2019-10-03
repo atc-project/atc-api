9fea4bf39a2b03306f3e8608c61c0291d591198c
m.aksenov <m.aksenov@bi.zone>
2019-10-03 15:40:48 +0300

    Add detection rule import

--- tests/dr2api.py
+++ tests/dr2api.py
@@ -1,22 +1,25 @@
 import requests
 import yaml
+from atcutils import ATCutils
+import os
 
 def export_dr(path_to_dr):
-    with open(path_to_dr, 'r') as stream:
-        try:
-            dr = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            raise
 
-
+    dr = ATCutils.read_yaml_file(path_to_dr)
+    dn = ATCutils.main_dn_calculatoin_func(path_to_dr)
+    dr['data_needed_names'] = dn
     r = requests.post('http://127.0.0.1:8000/api/v0/atc/detectionrule/', data=dr)
     print(r)
 
 
 
 
+
+
 if __name__=='__main__':
-    export_dr('files/sysmon_ads_executable.yml')
+    path = '../detection_rules/sigma/rules/apt/'
+    for file in os.listdir(path):
+        export_dr(f'{path}/{file}')
+
 
 
